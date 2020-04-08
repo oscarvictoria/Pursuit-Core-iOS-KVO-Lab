cd34efcd118c31fe0197806b07aae3a89ef98f5c
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-04-07 22:18:53 -0400

    added UI elements

--- KVOLab/KVOLab/CreateUserViewController.swift
+++ KVOLab/KVOLab/CreateUserViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CreateUserViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
 
+    
 }
 
---
+++ KVOLab/KVOLab/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  KVOLab
+//
+//  Created by Oscar Victoria Gonzalez  on 4/7/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
